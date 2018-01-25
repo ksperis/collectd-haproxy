90d39e88f2b67d04d24f1ddf7780f6db9fa6a4dd
isaackuang <isaac.kuang@gmail.com>
2018-01-25 12:08:38 +0800

    fix grafana data too long

--- haproxy.py
+++ haproxy.py
@@ -350,7 +350,7 @@ def _format_dimensions(dimensions):
     """
 
     dim_pairs = ["%s=%s" % (k, v) for k, v in dimensions.iteritems()]
-    return "[%s]" % (",".join(dim_pairs))
+    "%s_%s" % (dimensions['type'], dimensions['proxy_name'])
 
 
 def _get_proxy_type(type_id):
